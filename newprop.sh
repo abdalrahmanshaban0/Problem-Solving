0a4005087695502c02e27f194b61852107401c91
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2024-01-15 16:16:03 +0200

    Add files via upload

---
+++ newprop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+read PName
+FName="$(echo $PName | sed -r 's/[ .]+/_/g')"
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+touch "$FName.cpp"
+printf "/*\nProblem Name : $PName \nAuthor: Abdalrahman Shaban\nDate: $dt\n*/\n\n" > "$FName.cpp"
+cat ~/Documents/ps/temp.cpp >> "$FName.cpp"
+vim "$FName.cpp"
