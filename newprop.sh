29827bdaf17dfd4c8a17f7f7dc512d6c01561f7b
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2024-01-15 16:17:12 +0200

    Update newprop.sh

--- newprop.sh
+++ newprop.sh
@@ -4,5 +4,5 @@ FName="$(echo $PName | sed -r 's/[ .]+/_/g')"
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 touch "$FName.cpp"
 printf "/*\nProblem Name : $PName \nAuthor: Abdalrahman Shaban\nDate: $dt\n*/\n\n" > "$FName.cpp"
-cat ~/Documents/ps/temp.cpp >> "$FName.cpp"
+cat ~/Documents/Problem_solving/temp.cpp >> "$FName.cpp"
 vim "$FName.cpp"
