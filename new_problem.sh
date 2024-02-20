92bdf598d3e3a32ca918519da998d4891055c32d
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2024-02-20 13:27:49 +0200

    update new_problem.sh
    Adding support for clipboard in WSL

--- new_problem.sh
+++ new_problem.sh
@@ -4,5 +4,12 @@ FName="$(echo $PName | sed -r 's/[ .()?''""&/\-]+/_/g')"
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 touch "$FName.cpp"
 printf "/*\nProblem Name : $PName \nAuthor: Abdalrahman Shaban\nDate: $dt\n*/\n\n" > "$FName.cpp"
-cat ~/Documents/Problem-Solving/temp.cpp >> "$FName.cpp"
+
+#for linux 
+#cat ~/Documents/Problem-Solving/temp.cpp >> "$FName.cpp"
+
+#for wsl
+cat /mnt/c/Users/Abdalrahman/Documents/GitHub/Problem-Solving/temp.cpp >> "$FName.cpp"
+dos2unix "$FName.cpp"
+
 vim "$FName.cpp"
