d16e66c7d66072d95b974833df6566c44ba1a95e
Abdalrahman Shaban <abdalrahmanshaban52@gmail.com>
2024-03-02 01:46:12 +0200

    Create E_Hierarchy.cpp

---
+++ phase2/Sheet_Graph_DFS_BFS_1/E_Hierarchy.cpp
@@ -0,0 +1,97 @@
+/*
+Problem Name : E - Hierarchy 
+Author: Abdalrahman Shaban
+Date: 01/03/2024 22:26:58
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<long long> vi;
+typedef pair<ll, ll> pi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+
+const ll MOD = 1000000007;
+const ll inf = LONG_LONG_MAX;
+
+ll gcd(ll a, ll b) { return a ? gcd(b % a, a) : b; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll nCr(ll n, ll r) { if (r == 0 || n == r) return 1; return nCr(n - 1, r - 1) * n / r;}
+
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+}
+
+void Fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+const int N = 1e5+5;
+vector<vector<int>> adj(N);
+vector<bool> vis(N);
+vector<int> topo;
+
+void dfs(int nd){
+    vis[nd] = 1;
+    for(auto& child : adj[nd]){
+        if(!vis[child]){
+            dfs(child);
+        }
+    }
+    topo.push_back(nd);
+}
+
+void solve(){
+    int n, k; cin >> n >> k; 
+    for(int i = 1; i <= k; i++){
+        int w; cin >> w;
+        for(int j = 0; j < w; j++){
+            int sd; cin >> sd;
+            adj[i].push_back(sd);
+        }
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(!vis[i])
+            dfs(i);
+    }
+
+    reverse(all(topo));
+
+//    for(auto& i : topo) cout << i << ' '; cout << endl; 
+
+    vector<int> boss(n+1);
+    int p = 0;
+    for(auto& i : topo){
+        boss[i] = p;
+        p = i;
+    }
+
+    for(int i = 1; i <= n; i++){
+        cout << boss[i] << endl;
+    }
+}
+
+int main() {
+    Fast();
+    file();
+    int t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
