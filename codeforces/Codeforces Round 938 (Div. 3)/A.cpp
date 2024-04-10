eae616fc588089f3fe7ae87ce01c231a54448bd4
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2024-04-10 02:40:47 +0200

    Codeforces Round 938 div 3

---
+++ codeforces/Codeforces Round 938 (Div. 3)/A.cpp
@@ -0,0 +1,63 @@
+/*
+Problem Name : A 
+Author: Abdalrahman Shaban
+Date: 09/04/2024 11:46:12
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<long long> vi;
+typedef pair<ll, ll> pi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+
+#define endl '\n'
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+
+const ll MOD = 1000000007;
+const ll inf = LONG_LONG_MAX;
+
+ll gcd(ll a, ll b) { return a ? gcd(b % a, a) : b; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll nCr(ll n, ll r) { if (r == 0 || n == r) return 1; return nCr(n - 1, r - 1) * n / r;}
+
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+}
+
+void Fast() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+void solve(){
+    int n, a, b; cin >> n >> a >> b;
+    if(2*a < b){
+        cout << n*a;
+    }
+    else{
+        cout << n/2 * b + n%2 * a;
+    }
+    cout << endl;
+}
+
+int main() {
+    Fast();
+    file();
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
