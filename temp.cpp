1b59b0b950f7136915199c3c63c644c23016c4eb
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2024-06-07 21:36:11 +0300

    some changes in temp

--- temp.cpp
+++ temp.cpp
@@ -7,13 +7,18 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<long long> vi;
 typedef pair<ll, ll> pi;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
-
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+/*
+   order_of_key (k) : Number of items strictly smaller than k .
+   find_by_order(k) : K-th element in a set (counting from zero).
+*/
 #define endl '\n'
 #define YES cout << "YES\n"
 #define NO cout << "NO\n"
 #define all(x) x.begin(), x.end()
 #define allr(x) x.rbegin(), x.rend()
+void vin(vector<ll>& v) {for(auto& i : v) cin >> i;}
 
 const ll MOD = 1000000007;
 const ll inf = LONG_LONG_MAX;
@@ -21,6 +26,15 @@ const ll inf = LONG_LONG_MAX;
 ll gcd(ll a, ll b) { return a ? gcd(b % a, a) : b; }
 ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 ll nCr(ll n, ll r) { if (r == 0 || n == r) return 1; return nCr(n - 1, r - 1) * n / r;}
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(NULL), right(NULL) { }
+    TreeNode(int v) : val(v), left(NULL), right(NULL) { }
+    TreeNode(int v, TreeNode* l, TreeNode* r) : val(v), left(l), right(r) { }
+};
 
 void file() {
 #ifndef ONLINE_JUDGE
@@ -34,10 +48,8 @@ void Fast() {
     cout.tie(NULL);
 }
 
-//int dx[] = {0,0,1,-1};
-//int dy[] = {1,-1,0,0};
-//int dx[] = {0,0,1,-1,-1,-1,1,1};
-//int dy[] = {1,-1,0,0,1,-1,-1,-1};
+//int dx[] = {0, 0, 1,-1, -1,-1, 1, 1};
+//int dy[] = {1,-1, 0, 0,  1,-1,-1,-1};
 
 void solve(){
      
@@ -46,10 +58,6 @@ void solve(){
 int main() {
     Fast();
     file();
-    int t = 1;
-    cin >> t;
-    while(t--){
-        solve();
-    }
+    solve(); 
     return 0;
 }
