b2cd7115d7a3fe2042598067641d7cfd1935e94d
abdalrahmanshaban0 <abdalrahmanshaban52@gmail.com>
2024-07-02 22:53:33 +0300

    update temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,3 +1,9 @@
+/*
+Problem Name : Flight Discount 
+Author: Abdalrahman Shaban
+Date: 02/07/2024 22:27:47
+*/
+
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
@@ -27,7 +33,7 @@ ll gcd(ll a, ll b) { return a ? gcd(b % a, a) : b; }
 ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 ll nCr(ll n, ll r) { if (r == 0 || n == r) return 1; return nCr(n - 1, r - 1) * n / r;}
 
-vector<ll> factorization(ll num){
+vector<ll> primefactors(ll num){
     vector<ll> ret;
     for(ll i = 2; i*i <= num; i++){
         if(num%i == 0) ret.push_back(i);
@@ -36,14 +42,24 @@ vector<ll> factorization(ll num){
     if(num > 1) ret.push_back(num);
     return ret;
 }
-
+vector<ll> divisors(ll num){
+    vector<ll> ret;
+    for(ll i = 1; i*i <= num; i++){
+        if(num%i == 0){
+            ret.push_back(i);
+            if(num/i != i){
+                ret.push_back(num/i);
+            }
+        }
+    }
+    return ret;
+}
 ll fpower(ll x, ll y, ll M){
     if(y == 0) return 1;
     ll val = fpower(x, y/2, M) % M;
     val = (val*val)%M;
     return (val*(y&1 ? x : 1))%M;
 }
-
 ll modInverse(ll A, ll M){
     ll g = gcd(A, M);
     if (g != 1) return -1;
@@ -57,6 +73,14 @@ struct TreeNode{
     TreeNode() : val(0), left(NULL), right(NULL) { }
     TreeNode(int v) : val(v), left(NULL), right(NULL) { }
     TreeNode(int v, TreeNode* l, TreeNode* r) : val(v), left(l), right(r) { }
+};
+struct edge{
+	int from, to, w; //from, if you want to backtrack the path
+	edge(int t, int we) : to(t), w(we) { }
+	edge(int f, int t, int we) : from(f), to(t), w(we) { }
+	bool operator < (const edge& obj) const {
+		return(w > obj.w);  //return greater to make the smaller is the top
+	}
 };
 
 void file() {
@@ -77,7 +101,7 @@ void Fast() {
 
 
 void solve(){
-     
+    
 }
 
 int main() {
